257db8756e605b4f289132db47ee26bcf0b4926b
emily <fabemilyr@gmail.com>
2023-07-19 20:34:48 -0500

    added title page

--- time-tracker/src/App.tsx
+++ time-tracker/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, {createContext, useContext, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+
+// interface UserContextType {
+//   name:string;
+//   username:string;
+//   password:string;
+//   setName: (name:string) => void;
+//   setUsername: (username:string) => void;
+//   setPassword: (password:string) => void;
+// }
+// const UserContext = createContext<UserContextType>({
+//   name:"",
+//   username:"",
+//   password:"",
+//   setUsername: () => {},
+//   setPassword: ()=>{}
+// });
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <Login/>
+      </div>
     </div>
   );
 }
---
+++ time-tracker/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import Title from "../components/Title";
+
+export default function Login() {
+  return (
+    <div>
+      <div className="mt-10">
+        <Title />
+      </div>
+    </div>
+  );
+}
